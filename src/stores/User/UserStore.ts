597b41be7dbb0108395e4a9f285c30234f035165
MoncoJS <moncojs@gmail.com>
2025-12-19 16:06:05 +0700

    Post Users

--- src/stores/User/UserStore.ts
+++ src/stores/User/UserStore.ts
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia'
 
-import { getUser } from '@/stores/User/UserService'
+import { getUser, addUser, updateUser, deleteUser } from '@/stores/User/UserService'
 import type { User } from '@/stores/User/UserInterface'
 
 export const useUserStore = defineStore('user', {
@@ -22,22 +22,31 @@ export const useUserStore = defineStore('user', {
     selectUser(user: User) {
       this.selectedUser = user
     },
-    addUser(user: User) {
-      this.users.unshift(user)
-      this.selectedUser = user
-    },
-    updateUser(originalUser: User, newUser: User) {
-      const idx = this.users.indexOf(originalUser)
-      if (idx !== -1) {
-        this.users[idx] = newUser
-        this.selectedUser = newUser
+    async addUser(user: User) {
+      const newUser = await addUser(user)
+      if (newUser && newUser.first_name) {
+         this.users.unshift(newUser)
+         this.selectedUser = newUser
       }
     },
-    deleteUser(user: User) {
-      const idx = this.users.indexOf(user)
-      if (idx !== -1) this.users.splice(idx, 1)
-      if (this.selectedUser === user) {
-        this.selectedUser = this.users[0] || null
+    async updateUser(originalUser: User, newUser: User) {
+      const updatedUser = await updateUser(newUser)
+      if (updatedUser && updatedUser.user_id) {
+        const idx = this.users.indexOf(originalUser)
+        if (idx !== -1) {
+          this.users[idx] = updatedUser
+          this.selectedUser = updatedUser
+        }
+      }
+    },
+    async deleteUser(user: User) {
+      const deletedUser = await deleteUser(user)
+      if (deletedUser) { // Assuming backend returns success even if empty user
+        const idx = this.users.indexOf(user)
+        if (idx !== -1) this.users.splice(idx, 1)
+        if (this.selectedUser === user) {
+          this.selectedUser = this.users[0] || null
+        }
       }
     },
   },
